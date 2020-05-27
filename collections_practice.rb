a9fa3cabf269e440ab90fa819973c591329382ab
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 20:30:32 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a, b|
+    if a<b
+      1
+    else
+      0
+    end
+  }
+end
+
+def sort_array_char_count(array)
+  array.sort{|a, b|
+    if a.length > b.length
+      1
+    else
+      0
+    end
+  }
+end
+
+def swap_elements(array)
+  aux = array[2]
+  array[2] = array[1]
+  array[1] = aux
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |element| element[2]="$" }
+end
+
+def find_a(array)
+  array.select{|element| element.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject{|sum, element| sum += element}
+end
+
+def add_s(array)
+  array.each_with_index{|element, index|
+    if index!=1
+      element << "s"
+    end
+  }
+end
